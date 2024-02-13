47b1d099bcdaa2fe8b11982597db9801d60a25cb
OtlotlengMajuja <156651807+OtlotlengMajuja@users.noreply.github.com>
2024-02-13 14:28:57 +0200

    Started and completed my blackjack game

---
+++ index.js
@@ -0,0 +1,65 @@
+let player = {
+    name: "You",
+    chips: 150
+}
+
+let cards = []
+let sum = 0
+let hasBlackjack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": R" + player.chips
+// After receiving initial cards, once "New Card" is selected, more than one card is drawn and it's a struggle to 
+function getRandomCard() {
+    return Math.floor( Math.random()*13) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}   
+
+function startGame() {
+    isAlive = true 
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+     cardsEl.textcontent = "Cards: " 
+    for (let i = 0; i < cards.length; i++) {
+         cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+       message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+       message = "You've got Blackjack!"
+       hasBlackjack = true
+    } else {
+       message = "You're out of the game!"
+       isAlive = false
+    } 
+    messageEl.textContent = message
+}
+
+function newCard() {
+  if (isAlive === true && hasBlackjack === false) {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
+  }
+  
+}
